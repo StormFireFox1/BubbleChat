963ef8875710de3574495274466abd77b2039831
StormFireFox1 <matei@gardus.eu>
2018-08-01 14:53:32 -0700

    Add bubble routes and prep chatroom init

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var fs = require('fs');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var clusterRouter = require('./routes/clusters');
+var bubbleRouter = require('./routes/bubbles');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/clusters', clusterRouter);
+app.use('/bubbles', bubbleRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
